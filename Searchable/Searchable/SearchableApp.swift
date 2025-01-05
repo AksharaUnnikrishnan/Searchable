e05584e173e2cafda9f199cacdd9a505a1827146
Akshara Unnikrishnan <aksharanair.u@gmail.com>
2025-01-05 14:41:46 +0200

    Initial commit

---
+++ Searchable/Searchable/SearchableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableApp.swift
+//  Searchable
+//
+//  Created by Akshara Unnikrishnan on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
